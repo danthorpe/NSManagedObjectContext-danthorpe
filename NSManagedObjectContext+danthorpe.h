68df16669b691ec9779bbc974ecce904c00e920b
Dan Thorpe <dan@bravenewtalent.com>
2012-04-17 11:35:02 +0100

    Initial addition of category

---
+++ NSManagedObjectContext+danthorpe.h
@@ -0,0 +1,13 @@
+//
+//  NSManagedObjectContext+danthorpe.h
+//  Talent
+//
+//  Created by Daniel Thorpe on 17/04/2012.
+//  Copyright (c) 2012 Brave New Talent. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+@interface NSManagedObjectContext (danthorpe)
+
+@end
---
+++ NSManagedObjectContext+danthorpe.m
@@ -0,0 +1,13 @@
+//
+//  NSManagedObjectContext+danthorpe.m
+//  Talent
+//
+//  Created by Daniel Thorpe on 17/04/2012.
+//  Copyright (c) 2012 Brave New Talent. All rights reserved.
+//
+
+#import "NSManagedObjectContext+danthorpe.h"
+
+@implementation NSManagedObjectContext (danthorpe)
+
+@end
